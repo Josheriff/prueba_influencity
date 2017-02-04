c2b2169e5bfa0f522e9ce88bcbae977cc1080468
Jose <jakermater@gmail.com>
2017-02-04 15:25:48 +0100

    Added express and the basic management for /api/user/:username

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
-var twitter_scrapper = require ('./scrapper');
-twitter_scrapper("InfluencityES", function(err, data){
-    console.log("Scraping Results:");
-    console.log(err ? err : data);
-    console.log("----------------------------------");
-})
+var twitter_scrapper = require ('./scrapper'),
+    express = require('express');
+
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+app.get('/api/user/:username', function (req, res) {
+    twitter_scrapper(req.params.username, function(err, data){
+        res.send(data);
+    });
+});
+
+app.listen(port, function () {
+    console.log(`Influencity test running on port ${port}`);
+});
