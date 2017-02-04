d33aa9d7513fed0311c293ff62280c5a528a1340
Jose <jakermater@gmail.com>
2017-02-04 16:49:18 +0100

    mongodb setup and schema

--- server.js
+++ server.js
@@ -1,24 +1,61 @@
-var twitter_scrapper = require ('./scrapper'),
-    express = require('express');
+var twitter_scrapper = require('./scrapper'),
+    config = require('./config'),
+    express = require('express'),
+    mongoose = require('mongoose');
 
 var app = express();
 
 var port = process.env.PORT || 3000;
 
+// MONGOOSE SCHEMA
+var userSchema = new mongoose.Schema({
+    _id: String,
+    date: String,
+    data: {
+        screen_name: String,
+        followers_count: Number,
+        following_count: Number,
+        photo_url: String,
+        bio: String,
+        total_tweets: Number
+    }
+});
+
+//MONGOOSE MODELS
+
+var userModel = mongoose.model('userModel',userSchema);
+
+//MONGOOSE CONNECTION
+
+mongoose.connect(config.mongoUri, function (error) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log('mongo connected');
+    }
+});
+
 //MIDDLEWARE
 
 app.use(function(req, res, next) {
-   res.header("Access-Control-Allow-Origin", "*");
-   res.header('Access-Control-Allow-Methods', 'GET'); // right now, only get method allowed
-   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-   next();
- });
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET'); // right now, only get method allowed
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 // ROUTES
 app.get('/api/user/:username?', function (req, res) {
     if(req.params.username){
         twitter_scrapper(req.params.username, function(err, data){
-            err ? res.status(500).json({error: err}) : res.json(data);
+            if (err){
+                res.status(500).json({error:err});
+            } else {
+                // date parsed to ISO 8601 format easier to work with in the future
+
+                //var nowIso = new Date().toISOString();
+                res.json(data);
+            }
         });
     } else {
         //err 418 because is much better than 404 or 400 and keep you awake.
