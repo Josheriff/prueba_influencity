ab13e4dcfefb8bf4be8e18c45a2936f4c1817e31
Jose <jakermater@gmail.com>
2017-02-04 15:49:47 +0100

    CORS added to all routes

--- server.js
+++ server.js
@@ -5,6 +5,16 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
+//MIDDLEWARE
+
+app.use(function(req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header('Access-Control-Allow-Methods', 'GET'); // right now, only get method allowed
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   next();
+ });
+
+// ROUTES
 app.get('/api/user/:username?', function (req, res) {
     if(req.params.username){
         twitter_scrapper(req.params.username, function(err, data){
